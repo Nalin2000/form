2d977a65e7b20bf2461e702aa03286b692a4c3a6
Nalin2000 <nalinsaytongchang@gmail.com>
2020-08-02 19:58:27 +0700

    formate

--- autoTab.js
+++ autoTab.js
@@ -15,41 +15,41 @@ function autoTab(obj) {
     }
 }
 
-function CheckNum(){
-    if (event.keyCode < 48 || event.keyCode > 57){
-          event.returnValue = false;
-         }
+function CheckNum() {
+    if (event.keyCode < 48 || event.keyCode > 57) {
+        event.returnValue = false;
     }
+}
 
 function Post(obj) {
-        var pattern = new String("_____"); // กำหนดรูปแบบในนี้
-        var returnText = new String("");
-        var obj_l = obj.value.length;
-        var obj_4 = obj_l - 1;
-        for (i = 0; i < pattern.length; i++) {
-            if (obj_4 == i && pattern.charAt(i + 1) == pattern_ex) {
-                obj.value = returnText;
-            }
-        }
-        if (obj_l >= pattern.length) {
-            obj.value = obj.value.substr(0, pattern.length);
+    var pattern = new String("_____"); // กำหนดรูปแบบในนี้
+    var returnText = new String("");
+    var obj_l = obj.value.length;
+    var obj_4 = obj_l - 1;
+    for (i = 0; i < pattern.length; i++) {
+        if (obj_4 == i && pattern.charAt(i + 1) == pattern_ex) {
+            obj.value = returnText;
         }
     }
+    if (obj_l >= pattern.length) {
+        obj.value = obj.value.substr(0, pattern.length);
+    }
+}
 
-    
-    function Tel(obj) {
-        var pattern = new String("__-____-____"); // กำหนดรูปแบบในนี้
-        var pattern_ex = new String("-"); // กำหนดสัญลักษณ์หรือเครื่องหมายที่ใช้แบ่งในนี้
-        var returnText = new String("");
-        var obj_l = obj.value.length;
-        var obj_l2 = obj_l - 1;
-        for (i = 0; i < pattern.length; i++) {
-            if (obj_l2 == i && pattern.charAt(i + 1) == pattern_ex) {
-                returnText += obj.value + pattern_ex;
-                obj.value = returnText;
-            }
-        }
-        if (obj_l >= pattern.length) {
-            obj.value = obj.value.substr(0, pattern.length);
+
+function Tel(obj) {
+    var pattern = new String("__-____-____"); // กำหนดรูปแบบในนี้
+    var pattern_ex = new String("-"); // กำหนดสัญลักษณ์หรือเครื่องหมายที่ใช้แบ่งในนี้
+    var returnText = new String("");
+    var obj_l = obj.value.length;
+    var obj_l2 = obj_l - 1;
+    for (i = 0; i < pattern.length; i++) {
+        if (obj_l2 == i && pattern.charAt(i + 1) == pattern_ex) {
+            returnText += obj.value + pattern_ex;
+            obj.value = returnText;
         }
     }
+    if (obj_l >= pattern.length) {
+        obj.value = obj.value.substr(0, pattern.length);
+    }
+}
